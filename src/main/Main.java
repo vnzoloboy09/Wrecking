fd575cd8cea48cab550c3e6f1cdf347062a5d29a
Thanh Dat <vnzoloboy09@gmail.com>
2025-09-25 14:25:06 +0700

    [INIT] (Dat) Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setTitle("Brick");
+        window.setSize(800, 600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null );
+
+        window.setVisible(true);
+    }
+}
